461d7411317f583340fc6410b5eadf36fffbdb77
Ruan Duarte <ruanduarte0300@gmail.com>
2023-05-24 13:33:55 -0300

    settings at env

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 async function mongooseConnect() {
   try {
-    await mongoose.connect("mongodb+srv://ruanduarte0300:sMqE9gBRD29G9JTQ@zrp-dev.qic46rr.mongodb.net/");
+    const password = process.env.PASSWORD_BD
+    await mongoose.connect(`mongodb+srv://ruanduarte0300:${password}@zrp-dev.qic46rr.mongodb.net/`);
 
     console.log("DB connect");
   } catch (error) {
